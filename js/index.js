4302c48a31fb9d46a25fb37ed612248c83b9713d
Aviv <aviv.dror@gmail.com>
2021-12-08 04:33:35 +0100

    js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const startBtn = document.querySelector(".start");
+const firstQuestion = document.querySelector(".first");
+
+startBtn.addEventListener("click", () => {
+  if (firstQuestion.style.display === "none") {
+    firstQuestion.style.display = "block";
+  } else {
+    firstQuestion.style.display = "none";
+  }
+});
