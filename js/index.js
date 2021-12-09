fa3a407acf3ead9b6985a854c5102f772a4507ba
Aviv <aviv.dror@gmail.com>
2021-12-09 01:00:18 +0100

    setting first condition

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 "use strict";
 
 const startBtn = document.querySelector(".start");
-const firstQuestion = document.querySelector(".first");
+const panel = document.querySelector(".panel");
+const userBtn = document.querySelector(".user__btn");
+let message = document.querySelector(".message");
+let userInput = document.querySelector(".user__input");
 
 startBtn.addEventListener("click", () => {
-  if (firstQuestion.style.display === "none") {
-    firstQuestion.style.display = "block";
+  panel.classList.toggle("show");
+  message.innerHTML =
+    " ok! first of all, let's see if your specimen is even an insect <br/>at all, or another type of arthropod. <br /><br />How many legs does it have (please type number)?";
+});
+
+let legNumber = userInput.value;
+let sixLegs = false;
+
+if (legNumber == "six") {
+  sixLegs = true;
+}
+
+userBtn.addEventListener("click", () => {
+  if (sixLegs) {
+    message.innerHTML = "great!";
   } else {
-    firstQuestion.style.display = "none";
+    message.innerHTML = "stellar!";
   }
 });
