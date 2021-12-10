e73bb1e6efe62a0790a347614e32ee4467b95de8
Aviv <aviv.dror@gmail.com>
2021-12-10 03:46:26 +0100

    second question line

--- js/index.js
+++ js/index.js
@@ -11,7 +11,8 @@ startBtn.addEventListener("click", () => {
     " ok! first of all, let's see if your specimen is even an insect <br/>at all, or another type of arthropod. <br /><br />How many legs does it have?";
 });
 
-let firstBtn = document.querySelector(".user__btn");
+let firstBtn = document.querySelector(".first");
+let secondBtn = null;
 
 firstBtn.addEventListener("click", () => {
   let legNumber = userInput.value;
@@ -25,12 +26,42 @@ firstBtn.addEventListener("click", () => {
       legNumber === "six" ||
       legNumber === "6"
     ) {
-      message.innerHTML = "stellar! <br/>how many body parts does it have?";
+      message.innerHTML = "stellar! <br/>how many body joints does it have?";
       isLegs = true;
+      firstBtn.classList.toggle("second");
+      break;
     } else {
       message.innerHTML =
         "Your specimen is not an insect. <br/> <br/>it is most likely another type of arthropod.";
       isLegs = true;
     }
   }
+  secondBtn = document.querySelector(".second");
+
+  secondBtn.addEventListener("click", () => {
+    let bodyNumber = userInput.value;
+    let isBody = false;
+    while (!isBody) {
+      if (
+        bodyNumber === "" ||
+        bodyNumber === null ||
+        bodyNumber === undefined
+      ) {
+        message.innerHTML = "Please enter number of body joints";
+        isBody = true;
+      } else if (
+        bodyNumber === "Three" ||
+        bodyNumber === "three" ||
+        bodyNumber === "3"
+      ) {
+        message.innerHTML =
+          "Amazing! <br/> Your specimen is an insect for sure! Now we can start classifying what type of insect <br/><br/>does your specimen have wings?";
+        isBody = true;
+      } else {
+        message.innerHTML =
+          "Your specimen is not an insect. <br/> <br/>it is most likely another type of arthropod.";
+        isBody = true;
+      }
+    }
+  });
 });
